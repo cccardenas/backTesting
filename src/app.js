63da951d6ab53b967f880f5848117822981d0b6d
cristianCardenas23 <twstercamilo@gmail.com>
2023-10-01 21:44:02 -0500

    regisnr

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const BodyParser = require("body-parser");
 const UserRoutes = require("./routes/UserRoutes");
+
+// Use express.json() middleware to parse JSON request bodies
+app.use(express.json());
 
 app.use("/api/usuario", UserRoutes);
 
