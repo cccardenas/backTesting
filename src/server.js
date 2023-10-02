45c683350bee438e7de73424b0e3810ef05718f5
cristianCardenas23 <twstercamilo@gmail.com>
2023-10-01 22:26:15 -0500

    canisdasd

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require("./app");
 const db = require("./db/DBConnection");
 
-app.set("port", 8080);
+app.set("port", process.env.PORT || 8080);
 app.listen(app.get("port"), () => {
   console.log("Servidor corriendo en el puerto: " + app.get("port") + "   V1.5.2805");
 });
